a1703a5478c823fe39015e46bf37f8eebd02ffb7
Pedro Santos <zer0games2@hotmail.com>
2019-04-24 14:44:08 -0300

    Enabled Debugging

--- main.js
+++ main.js
@@ -69,14 +69,7 @@ function start(data)
     {
         let now;
         document.getElementById("categoria").innerHTML = data.dupla[0].categoria;
-        if (data.dupla[0].tempofim != "")
-        {
-            now = data.dupla[0].tempofim;
-        }
-        else
-        {
-            now = new Date();
-        }
+        now = new Date();
         let received = new Date(data.dupla[0].tempoini);
         let diff = (now -  received) ;
         startTime = received;
